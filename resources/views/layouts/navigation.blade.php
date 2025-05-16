e9e7f99530ee03069fe041eede217319df7de6fd
Cassandre <mariecassandrea2005@gmail.com>
2025-05-16 14:00:50 +0000

    Création de sous-menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -57,7 +57,7 @@
                         {{ __('Partenaires') }}
                     </x-nav-link>
                 </div>
-                {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('galerie.index')" :active="request()->routeIs('galerie.index')">
                         {{ __('Galerie') }}
                     </x-nav-link>
@@ -66,32 +66,32 @@
                     <x-nav-link :href="route('video.index')" :active="request()->routeIs('video.index')">
                         {{ __('Vidéos') }}
                     </x-nav-link>
-                </div> --}}
-                <div x-data="{ open: false }" class="relative hidden sm:ms-10 sm:flex">
-                    <button @click="open = !open"
-                        class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 focus:outline-none transition">
-                        Médias
-                        <svg class="ml-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M7 7l3-3 3 3m0 6l-3 3-3-3" />
-                        </svg>
-                    </button>
-
-                    <div x-show="open" @click.away="open = false"
-                        class="absolute z-10 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                        <div class="py-1">
-                            <x-nav-link :href="route('galerie.index')" :active="request()->routeIs('galerie.index')"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                {{ __('Galerie') }}
-                            </x-nav-link>
-                            <x-nav-link :href="route('video.index')" :active="request()->routeIs('video.index')"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                {{ __('Vidéos') }}
-                            </x-nav-link>
-                        </div>
-                    </div>
                 </div>
+                <li>
+                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                        class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Médias
+                        <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 4 4 4-4" />
+                        </svg></button>
+                    <!-- Dropdown menu -->
+                    <div id="dropdownNavbar"
+                        class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
+                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                            <li>
+                                <x-nav-link :href="route('galerie.index')" :active="request()->routeIs('galerie.index')" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    {{ __('Galerie-Photos') }}
+                                </x-nav-link>
+                            </li>
+                            <li>
+                                <x-nav-link :href="route('video.index')" :active="request()->routeIs('video.index')" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    {{ __('Vidéos') }}
+                                </x-nav-link>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
 
             </div>
 
