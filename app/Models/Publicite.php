5ea4e2920f5aaf3bb11b18a1077541a4643fd9ab
Cassandre <mariecassandrea2005@gmail.com>
2025-05-16 11:56:47 +0000

    Ajout de media library au models publicite et video

--- app/Models/Publicite.php
+++ app/Models/Publicite.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Publicite extends Model
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Publicite extends Model implements HasMedia
 {
     /** @use HasFactory<\Database\Factories\PublicitesFactory> */
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = [
         'nom',
@@ -17,6 +22,24 @@ class Publicite extends Model
     ];
 
 
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('preview')
+            ->fit(Fit::Contain, 300, 300)
+            ->nonQueued();
+
+        $this->addMediaConversion('small')
+            ->width(400)
+            ->nonQueued();
+
+        $this->addMediaConversion('normal')
+            ->width(800)
+            ->nonQueued();
+
+        $this->addMediaConversion('large')
+            ->width(1200)
+            ->nonQueued();
+    }
     public function user()
 {
     return $this->belongsTo(User::class);
